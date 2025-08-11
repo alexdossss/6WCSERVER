aa2c070e742859fed5778dd6091989f40a83b307
alexdossss <amanabat1927@gmail.com>
2025-08-11 11:01:21 +0800

    Initial commit

---
+++ express.js
@@ -0,0 +1,73 @@
+import express from 'express';
+const __dirname = import.meta.dirname;
+
+const app = express();
+app.use(express.static('public'));
+
+// Routes
+app.get('/', (req, res) =>{
+    res.sendFile(__dirname + '/pages/home.html')
+});
+
+app.get('/userPage', (req, res) =>{
+    res.sendFile(__dirname + '/pages/user.html')
+});
+
+app.get('/studentForm', (req, res) =>{
+    res.sendFile(__dirname + '/pages/student.html')
+});
+
+app.get('/adminForm', (req, res) =>{
+    res.sendFile(__dirname + '/pages/admin.html')
+});
+
+app.get('/user', (req,res) =>{
+    const userId = req.query.id;
+    const userName = req.query.name;
+    if (userId && userName){
+        req.send(`<html><body><h1>User ${userName}'s ID is : ${userId}</h1></body></html>`);
+    }else res.status(400).send(`User ID and name is required`);
+});
+
+
+// Get Func
+app.get('/getUser', (req,res) =>{
+    var response = {
+        firstName: req.query.firstName,
+        lastName: req.query.lastName,
+    }
+
+    console.log("Response is: ", response);
+    res.end(`Received Data ${JSON.stringify(response)}`)
+});
+
+app.get('/getStudent', (req,res) =>{
+    var response = {
+        studentId: req.query.studentId,
+        firstName: req.query.firstName,
+        lastName: req.query.lastName,
+        section: req.query.section
+    }
+
+    console.log("Response is: ", response);
+    res.end(`Received Data ${JSON.stringify(response)}`)
+});
+
+app.get('/getAdmin', (req,res) =>{
+    var response = {
+        adminId: req.query.adminId,
+        firstName: req.query.firstName,
+        lastName: req.query.lastName,
+        department: req.query.department
+    }
+
+    console.log("Response is: ", response);
+    res.end(`Received Data ${JSON.stringify(response)}`)
+});
+
+
+const server = app.listen(5000, () =>{
+    const host = server.address().address;
+    const port = server.address().port;
+    console.log(`Server running at http://${host}:${port}`);
+})
